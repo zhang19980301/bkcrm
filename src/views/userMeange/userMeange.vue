a8af23fca53fb6fbffdaad1aa02e43f3199663a2
zhangsuqi <381344750@qq.com>
2022-01-22 11:05:32 +0800

    用户管理的代码提交

--- src/views/userMeange/userMeange.vue
+++ src/views/userMeange/userMeange.vue
@@ -3,14 +3,25 @@
     <el-button type="primary">新建人员</el-button>
     <!-- 人员列表 -->
     <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
-      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
-      <el-table-column prop="address" label="地址"> </el-table-column>
+      <el-table-column prop="id" label="ID" width="180"> </el-table-column>
+      <el-table-column prop="loginName" label="登录名称" width="180"> </el-table-column>
+      <el-table-column prop="nicName" label="昵称"> </el-table-column>
       <el-table-column label="操作"> 
         <el-button>编辑</el-button>
         <el-button type="danger">删除</el-button>
       </el-table-column>
     </el-table>
+    <el-pagination
+      v-model:currentPage="currentPage"
+      :hide-on-single-page="total==1?true:false"
+      :page-sizes="[10, 50, 200, 500]"
+      :page-size="10"
+      layout="total, prev, pager, next, sizes, jumper"
+      :total="total"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    >
+    </el-pagination>
   </div>
 </template>
 
@@ -18,36 +29,39 @@
 export default {
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
-        }
-      ]
+      tableData: [],
+      currentPage:1,
+      pageSize: 10,
+      total: 0
     };
   },
-  methods: {},
   async created() {
-    let obj = await this.$HTTP({ url: "/getAllUsers", data: {pagelist: 10, pageNo: 1} })
-    console.log(obj)
-    // this.$
-  }
+    this.getAllUsers()
+  },
+  methods: {
+    /**
+     * 分页获取所有用户信息
+     */
+    async getAllUsers(){
+      let obj = await this.$HTTP({ url: "/getAllUsers", data: {pagelist: this.pageSize, pageNo: this.currentPage} })
+      this.total = obj.totle
+      this.tableData = obj.data
+    },
+    /**
+     * 切换一页展示多少条
+     */
+    async handleSizeChange(pageSize){
+      this.pageSize = pageSize
+      this.getAllUsers()
+    },
+    /**
+     * 更改当前的选中页数
+     */
+    async handleCurrentChange(currentPage){
+      this.currentPage = currentPage
+      this.getAllUsers()
+    }
+  },
 };
 </script>
 
